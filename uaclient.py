fbccff0d820c7717739d0fd2df0dac62a0b9d065
gegonmaz <gegonmaz@gmail.com>
2017-01-11 12:56:26 +0100

    metodo registrar implementado

--- uaclient.py
+++ uaclient.py
@@ -18,6 +18,20 @@ import logging
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
+# Tendremos que tener una variable donde se guarde el XML
+FicheroXML = sys.argv[1]
+# El metodo que se va a ejecutar
+Metodo = sys.argv[2]
+# Dependiendo del metodo, tendremos una opcion diferente
+if Metodo == REGISTER
+    TiempoExpiracion = sys.argv[3]
+elif Metodo == INVITE
+    Usuario = sys.argv[3]
+else Metodo == BYE
+    UsuarioDesconectado = sys.argv[3]
+# Creamos las variables de forma global para poder utilizarlas durante el prog.
+
 class UAClientHandler(ContentHandler):
 
 """
@@ -43,13 +57,8 @@ User Agent del cliente
     Vamos a tener que inicializar todos los elementos(etiquetas) del 
     diccionario creado anteriormente
     """
-
-    """
-    Tendremos que crear un archivo log; de modo que si cae el servicio,
-    se pueda saber que usuarios estaban contectados en cada momento
-    """
     def InicializacionElementos(self,name,attrs):
-        #Buscaremos las etiquetas en el diccionario
+        # Buscaremos las etiquetas en el diccionario
         if name in self.diccionarioConfig:
             Valores = {}
             for etiquetas in self.diccionarioConfig[name]:
@@ -57,12 +66,66 @@ User Agent del cliente
             listaNombres = {name: Valores}
             self.listaEtiquetas.append(listaNombres)
 
+
+    def get_tags(self):
+        # Nos tendrá que devolver la lista segun como este
+        return self.listaEtiquetas
+
+    """
+    Tendremos que crear un archivo log; de modo que si cae el servicio,
+    se pueda saber que usuarios estaban contectados en cada momento
+    """
+    def archivoLog(File, Message)
+        # Primero aseguramos si existe algún fichero ya
+        ficheroLectura = open(File, 'r')
+        lineas = fichero.readlines
+        ficheroLectura.close()
+        lineas.append(str(time.time()) + '' + Message)
+        # Escribimos en el fichero
+        ficheroEscritura = open(File, 'w')
+        ficheroEscritura(''.join(lienas))
+        ficheroEscritura.close()    
+
     """
     Una vez inicializadas, tendremos que crear alguna funcion que nos permita
     acceder a las etiquetas
     """
+    parser =make_parser()
+    cHandler = UAClientHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(FicheroXML)
 
+    """
+    Como vamos a tener que estar constantemente enviando los mensajes, 
+    crearemos un metodo para solo tener que llamar al metodo.
+    """
+    def envioMensajes(self, mensaje)
+        my_socket.send(bytes(mensaje, 'utf-8'))
 
+    """
+    El cliente se tiene que registrar, tendrá que hacer la invitación de la
+    llamada, realizar los ack al proxy y desconectarse, por tanto los podriamos
+    implementar como metodos.
+    """
+    def registrar(self, TiempoExpiracion):
+
+        if Metodo == 'REGITER':
+            c = 'Expires: ' + str(TiempoExpiracion) + '\r\n\r\n'
+            cabeceraRegistro = Metodo + 'sip: ' + 
+                               self.listaEtiquetas['uaserver']['puerto'] + 
+                               ' SIP/2.0\r\n'
+            lineaEnvio = cabeceraRegistro + c
+            self.send(lineaEnvio)
 
 if __name__ == "__main__":
 
+    # Tendremos que crear el socket 
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    # Esta vez en vez de anudarlo a un servidor, lo haremos al Proxy
+    my_socket.connect((diccionarioConfig['regproxy']['ip'], 
+                       int(diccionarioConfig['regproxy']['puerto']))
+
+    # El programa tendrá que actuar segun el metodo que haya recibido
+    
+    if len(sys.argv) != 4:
+        sys.exit('Usege: python uaclient.py config method option')
